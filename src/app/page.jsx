7a0d49563109fceb1e1aad65c165158b3908e6b4
ShirotaAyaka <yakka.as37@gmail.com>
2024-10-28 23:28:54 +0900

    エンドポイントを修正

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   const handleCheck = async () =>{
     try{
-      const res = await fetch('http://127.0.0.1:8000/check');
+      const res = await fetch('http://tech0-gen-7-step4-studentwebapp-pos-32-bddkf5emhxaphecs.eastus-01.azurewebsites.net/check');
       const data = await res.json();
       setMessage(data.message);
     }catch(error){
